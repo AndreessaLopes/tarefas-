e74324bcf73466b9d85faf50da52e06f1be1613f
Andressa <andressacaroline082011@gmail.com>
2025-05-24 14:43:14 -0300

    Adds info content and fixes className
    Adds a section displaying information such as post and comment counts to enhance user experience.
    
    Also, corrects a typo in the className application within the main container.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,11 @@
 import Head from "next/head";
 import styles from "../../styles/home.module.css";
 import Image from "next/image";
-
 import heroImg from "../../public/assets/hero.png";
 
 export default function Home() {
   return (
-    <div className="{styles.container}">
+    <div className={styles.container}>
       <Head>
         <title>Tarefas+ | Organize suas tarefas de forma fácil</title>
       </Head>
@@ -20,7 +19,23 @@ export default function Home() {
             priority
           />
         </div>
-        <h1 className={styles.title}>Sistema feito para você organizar <br/>seus estudos e tarefas</h1>
+        <h1 className={styles.title}>
+          Sistema feito para você organizar <br />
+          seus estudos e tarefas
+        </h1>
+
+        <div className={styles.infoContent}>
+          <section className={styles.box}>
+            <span>
+              + 12 posts
+            </span>
+          </section>
+          <section className={styles.box}>
+            <span>
+              90 comentários
+            </span>
+          </section>
+        </div>
       </main>
     </div>
   );
