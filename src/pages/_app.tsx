898a45559aa20b08315cdf91775333b340b4c6fb
Andressa <andressacaroline082011@gmail.com>
2025-06-29 11:14:40 -0300

    Adiciona autenticação com NextAuth e Google, cria página de dashboard

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,13 @@
 import { Header } from "@/components/header";
+import { SessionProvider } from "next-auth/react"
 import "../../styles/globals.css";
 import type { AppProps } from "next/app";
 
-export default function App({ Component, pageProps }: AppProps) {
+export default function App({ Component, pageProps}: AppProps) {
   return (
-    <>
+    <SessionProvider session={pageProps.session}>
       <Header />
       <Component {...pageProps} />
-    </>
+    </SessionProvider>
   );
 }
