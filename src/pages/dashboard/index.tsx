6890f6cf907d6c516dd86361d84820bc8f9d4981
Andressa <andressacaroline082011@gmail.com>
2025-06-29 12:27:10 -0300

    Adiciona autenticação no cabeçalho e cria página de dashboard com formulário de tarefas

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,17 +1,55 @@
+import { GetServerSideProps } from "next";
 import styles from "./styles.module.css";
 import Head from "next/head";
+import { getSession } from "next-auth/react";
+import {Textarea} from "../../components/textarea";
 
 export default function Dashboard() {
     return (
         <div className={styles.container}>
             <Head>
-                <title>
-                    Meu painel de tarefas
-                    </title>
+                <title>Meu painel de tarefas</title>
             </Head>
-            <h1>
-                Página Painel
-            </h1>
+                <main className={styles.main}>
+                    <section className={styles.content}>
+                        <div className={styles.contentForm}>
+                            <h1 className={styles.title}>
+                                Qual sua tarefa?
+                            </h1>
+
+                            <form>
+                                <Textarea 
+                                   placeholder="Digite sua tarefa aqui..."
+                                    
+                                />
+                                <div className={styles.checkboxArea}>
+                                    <input type="checkbox" className={styles.checkbox} />
+                                    <label>Deixar tarefa pública?</label>
+                                </div>
+                                <button type="submit" className={styles.button}>
+                                    Criar tarefa
+                                </button>
+                            </form>
+                        </div>
+                    </section>
+                </main>
         </div>
     );   
+}
+
+export const getServerSideProps: GetServerSideProps = async ({req}) => {
+
+    const session = await getSession({ req });
+    
+    if (!session?.user) {
+        return {
+            redirect: {
+                destination: "/",
+                permanent: false,
+            },
+        };
+    }
+    return {
+        props: {},
+    };
 }
