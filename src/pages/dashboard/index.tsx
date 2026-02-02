3555949195583862de35f592bc44981584e0b92f
Andressa <andressacaroline082011@gmail.com>
2026-02-02 10:24:04 -0300

    Adiciona ícones de compartilhamento e exclusão, e cria seção de tarefas no dashboard

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -3,6 +3,9 @@ import styles from "./styles.module.css";
 import Head from "next/head";
 import { getSession } from "next-auth/react";
 import {Textarea} from "../../components/textarea";
+import {FiShare2} from 'react-icons/fi'
+import {FaTrash} from 'react-icons/fa'
+
 
 export default function Dashboard() {
     return (
@@ -31,6 +34,26 @@ export default function Dashboard() {
                                 </button>
                             </form>
                         </div>
+                    </section>
+
+                    <section className={styles.taskContainer}>
+                        <h1>Minhas Tarefas</h1>
+                        <article className={styles.task}>
+                            <div className={styles.tagContainer}>
+                                <label className={styles.tag}>PÚBLICO</label>
+                                <button className={styles.shareButton}></button>
+                                    <FiShare2 size={22} color="#3183ff" />
+                            </div>
+                            <div className={styles.taskContent}>
+                                <p className={styles.taskTitle}>Estudar Next.js</p>
+                                <button className={styles.trashButton}>
+                                    <FaTrash size={24} color="#ea3140" />
+                                </button>
+                            </div>
+                        </article>
+
+                        
+
                     </section>
                 </main>
         </div>
